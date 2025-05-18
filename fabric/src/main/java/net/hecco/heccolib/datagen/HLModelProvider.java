54fbf03c728a7a6cb48e6751bba1174d60af3b4d
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-05-18 15:57:00 -0500

    Update HLModelProvider.java

--- fabric/src/main/java/net/hecco/heccolib/datagen/HLModelProvider.java
+++ fabric/src/main/java/net/hecco/heccolib/datagen/HLModelProvider.java
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 public abstract class HLModelProvider extends FabricModelProvider {
 
     private final String MOD_ID;
-    
+
     public HLModelProvider(String modId, FabricDataOutput output) {
         super(output);
         this.MOD_ID = modId;
