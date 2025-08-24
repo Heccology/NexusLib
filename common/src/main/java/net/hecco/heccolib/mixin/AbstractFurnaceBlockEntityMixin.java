9201747fad825f4b607cfb3f0703171750b730e3
Hecco <126524389+Hecco56@users.noreply.github.com>
2025-08-24 16:24:05 -0500

    asdfsdfasdf

--- common/src/main/java/net/hecco/heccolib/mixin/AbstractFurnaceBlockEntityMixin.java
+++ common/src/main/java/net/hecco/heccolib/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,5 +1,6 @@
 package net.hecco.heccolib.mixin;
 
+import net.hecco.heccolib.HeccoLib;
 import net.hecco.heccolib.lib.fuelRegistry.HLFuelRegistry;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
@@ -8,15 +9,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
     @Inject(method = "getFuel", at = @At("RETURN"), cancellable = true)
-    private static void heccolib$addFuels(CallbackInfoReturnable<Map<Item, Integer>> cir) {
-        Map<Item, Integer> map = new LinkedHashMap<>(cir.getReturnValue());
-        map.putAll(HLFuelRegistry.getFuels());
-        cir.setReturnValue(map);
+    private static void heccolib$getFuel(CallbackInfoReturnable<Map<Item, Integer>> cir) {
+        Map<Item, Integer> newMap = new HashMap<>();
+        HeccoLib.LOGGER.info("heccolib is cool");
+        HeccoLib.LOGGER.info(cir.getReturnValue().toString());
+        HeccoLib.LOGGER.info(HLFuelRegistry.getFuels().toString());
+        if (cir.getReturnValue() != null) {
+            newMap.putAll(cir.getReturnValue());
+        }
+        newMap.putAll(HLFuelRegistry.getFuels());
+        cir.setReturnValue(newMap);
     }
 }
