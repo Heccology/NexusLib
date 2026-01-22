8eda1c331169e304ccd8907cdf979c0c7a55cbef
Hecco <126524389+Hecco56@users.noreply.github.com>
2026-01-21 20:19:31 -0600

    Update ModIntegration.java

--- common/src/main/java/net/hecco/nexuslib/lib/compat/ModIntegration.java
+++ common/src/main/java/net/hecco/nexuslib/lib/compat/ModIntegration.java
@@ -1,9 +1,18 @@
 package net.hecco.nexuslib.lib.compat;
 
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public interface ModIntegration {
@@ -25,4 +34,11 @@ public interface ModIntegration {
     default String getDatapackName() {return null;}
 
     default void recipeGeneration(RecipeOutput output) {}
+
+    default Criterion<InventoryChangeTrigger.TriggerInstance> has(ResourceLocation id) {
+        return CriteriaTriggers.INVENTORY_CHANGED.createCriterion(new InventoryChangeTrigger.TriggerInstance(Optional.empty(), InventoryChangeTrigger.TriggerInstance.Slots.ANY, List.of(ItemPredicate.Builder.item().of(BuiltInRegistries.ITEM.get(id)).build())));
+    }
+    default Criterion<InventoryChangeTrigger.TriggerInstance> has(TagKey<Item> tag) {
+        return CriteriaTriggers.INVENTORY_CHANGED.createCriterion(new InventoryChangeTrigger.TriggerInstance(Optional.empty(), InventoryChangeTrigger.TriggerInstance.Slots.ANY, List.of(ItemPredicate.Builder.item().of(tag).build())));
+    }
 }
